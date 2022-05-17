4bad8ef2b19eebbb39c0d4c5e77b45f2b1c78483
Daniel K <danielu96@wp.pl>
2022-05-17 21:14:36 +0200

    zmiana brancha

--- das.js
+++ das.js
@@ -1,6 +1,7 @@
- class App extends React.Component {
- render () {
-   return(
-   <div><h1>REACT WELCOME<h1><div>)
- }
-blal alala 
+class App extends React.Component {
+  render() {
+    return (
+      <div><h1>REACT WELCOME po poprawkach</h1></div>)
+  }
+
+}
